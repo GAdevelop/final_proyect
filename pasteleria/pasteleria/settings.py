3c206778d8c53afe6e9f9e04d2b76f2f7c492f20
GAdevelop <gaston.ambrogi.developer@gmail.com>
2022-06-09 19:25:55 -0300

    Se agrega la app login_out

--- pasteleria/pasteleria/settings.py
+++ pasteleria/pasteleria/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'registro'
+    'registro',
+    'login_out',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
