298248986ceca0fef0012e999ac4ed9f3add7ae3
Bianca Paz <biancaconstanzapaz@gmail.com>
2022-07-03 22:53:31 -0300

    trabajamos con la app profiles

--- pasteleria/profiles/urls.py
+++ pasteleria/profiles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('viewProfile', views.viewProfile, name = 'view_profile'),
+    path('editProfile', views.editProfile, name = 'edit_profile'),
 
 ]
